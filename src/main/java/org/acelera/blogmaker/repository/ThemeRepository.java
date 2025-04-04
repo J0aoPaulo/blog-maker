97347727b131280ce0f9143c542b3a00f0c459cd
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2025-04-04 13:55:33 -0300

    feat: theme repository and minors changes

---
+++ src/main/java/org/acelera/blogmaker/repository/ThemeRepository.java
@@ -0,0 +1,9 @@
+package org.acelera.blogmaker.repository;
+
+import org.acelera.blogmaker.model.Theme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+}
