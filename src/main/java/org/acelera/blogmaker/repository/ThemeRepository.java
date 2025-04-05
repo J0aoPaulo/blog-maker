45e658dd2d8c1bae051e3a7a7d357a8af81467e1
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2025-04-05 09:33:10 -0300

    perf: add existByDescription

--- src/main/java/org/acelera/blogmaker/repository/ThemeRepository.java
+++ src/main/java/org/acelera/blogmaker/repository/ThemeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
+    boolean existsByDescription(String description);
 }
